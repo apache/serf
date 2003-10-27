786ff20f224b3700a575318712ad5df098c2c53f
Greg Stein <gstein@apache.org>
2003-10-27 09:47:25 +0000

    Refine the bucket types a bit and add some supporting functions.
    * branches/gen2/serf_bucket_types.h:
      (serf_bucket_request_create): new function to create a request bucket
      (serf_bucket_response_create): new function to create a response bucket
      (serf_bucket_response_status): new function to return the HTTP status
        code from the response.
      (serf_bucket_response_reason): new func to return the HTTP reason text
      (serf_bucket_response_protocol): new func to return the HTTP protocol
        version.
      (serf_bucket_aggregate_create): new func to create an aggregate bucket
      (serf_bucket_aggregate_prepend): new func to prepend a bucket into an
        aggregate's list of buckets.
      (serf_bucket_aggregate_append): new func to append a bucket onto an
        aggregate's list of buckets.
      (serf_bucket_file_create): new function to create a file bucket
      (serf_bucket_file_borrow): new function to borrow a reference to the
        bucket's file object.
      (serf_bucket_socket_create): new function to create a socket bucket
      (serf_bucket_mmap_create): new function to create an mmap bucket
    
      (serf_bucket_type_empty): removed
      (SERF_BUCKET_IS_EMPTY): removed
    
      (serf_bucket_type_pipe): removed
      (SERF_BUCKET_IS_PIPE): removed
    
    
    git-svn-id: https://svn.apache.org/repos/asf/commons/serf/branches/gen2@50 13f79535-47bb-0310-9956-ffa450edef68

--- serf_bucket_types.h
+++ serf_bucket_types.h
@@ -69,28 +69,90 @@
 extern "C" {
 #endif
 
+/* ==================================================================== */
+
 
 SERF_DECLARE_DATA extern const serf_bucket_type_t serf_bucket_type_request;
 #define SERF_BUCKET_IS_REQUEST(b) SERF_BUCKET_CHECK((b), request)
 
+SERF_DECLARE(serf_bucket_t *) serf_bucket_request_create(
+    const char *method,
+    const char *URI,
+    serf_bucket_t *body,
+    serf_bucket_alloc_t *allocator);
+
+
+/* ==================================================================== */
+
+
 SERF_DECLARE_DATA extern const serf_bucket_type_t serf_bucket_type_response;
 #define SERF_BUCKET_IS_RESPONSE(b) SERF_BUCKET_CHECK((b), response)
+
+SERF_DECLARE(serf_bucket_t *) serf_bucket_response_create(
+    serf_bucket_t *stream,
+    serf_bucket_alloc_t *allocator);
+
+/* ### hmm. these need to return APR_EAGAIN somehow. maybe 0 for the
+   ### integer functions and NULL for the reason? hmm. should probably
+   ### switch to apr_status_t so that we can return *any* network-related
+   ### error. (or parsing error or whatever)
+*/
+SERF_DECLARE(int) serf_bucket_response_status(serf_bucket_t *bkt);
+
+SERF_DECLARE(const char *) serf_bucket_response_reason(serf_bucket_t *bkt);
+
+SERF_DECLARE(int) serf_bucket_response_protocol(serf_bucket_t *bkt);
+
+
+/* ==================================================================== */
 
 
 SERF_DECLARE_DATA extern const serf_bucket_type_t serf_bucket_type_aggregate;
 #define SERF_BUCKET_IS_AGGREGATE(b) SERF_BUCKET_CHECK((b), aggregate)
 
-SERF_DECLARE_DATA extern const serf_bucket_type_t serf_bucket_type_empty;
-#define SERF_BUCKET_IS_EMPTY(b) SERF_BUCKET_CHECK((b), empty)
+SERF_DECALRE(serf_bucket_t *) serf_bucket_aggregate_create(
+    serf_bucket_alloc_t *allocator);
+
+SERF_DECLARE(void) serf_bucket_aggregate_prepend(
+    serf_bucket_t *aggregate_bucket,
+    serf_bucket_t *prepend_bucket);
+
+SERF_DECLARE(void) serf_bucket_aggregate_append(
+    serf_bucket_t *aggregate_bucket,
+    serf_bucket_t *append_bucket);
+
+
+/* ==================================================================== */
+
 
 SERF_DECLARE_DATA extern const serf_bucket_type_t serf_bucket_type_file;
 #define SERF_BUCKET_IS_FILE(b) SERF_BUCKET_CHECK((b), file)
 
+SERF_DECLARE(serf_bucket_t *) serf_bucket_file_create(
+    apr_file_t *file,
+    serf_bucket_alloc_t *allocator);
+
+SERF_DECLARE(apr_file_t *) serf_bucket_file_borrow(serf_bucket_t *bkt);
+
+
+/* ==================================================================== */
+
+
 SERF_DECLARE_DATA extern const serf_bucket_type_t serf_bucket_type_socket;
 #define SERF_BUCKET_IS_SOCKET(b) SERF_BUCKET_CHECK((b), socket)
 
-SERF_DECLARE_DATA extern const serf_bucket_type_t serf_bucket_type_pipe;
-#define SERF_BUCKET_IS_PIPE(b) SERF_BUCKET_CHECK((b), pipe)
+SERF_DECLARE(serf_bucket_t *) serf_bucket_socket_create(
+    apr_socket_t *socket,
+    serf_bucket_alloc_t *allocator);
+
+
+/* ==================================================================== */
+
+
+/* ### we probably don't need all three here. our memory model is a bit
+   ### special, so it may simply be that we have a bucket type that refers
+   ### to externally-managed memory
+*/
 
 SERF_DECLARE_DATA extern const serf_bucket_type_t serf_bucket_type_immortal;
 #define SERF_BUCKET_IS_IMMORTAL(b) SERF_BUCKET_CHECK((b), immortal)
@@ -101,8 +163,25 @@ SERF_DECLARE_DATA extern const serf_bucket_type_t serf_bucket_type_heap;
 SERF_DECLARE_DATA extern const serf_bucket_type_t serf_bucket_type_pool;
 #define SERF_BUCKET_IS_POOL(b) SERF_BUCKET_CHECK((b), pool)
 
+
+/* ==================================================================== */
+
+
+/* Note: this is always defined, but if APR doesn't have mmaps, then
+   the caller can never create an apr_mmap_t to pass to this function. */
+
 SERF_DECLARE_DATA extern const serf_bucket_type_t serf_bucket_type_mmap;
 #define SERF_BUCKET_IS_MMAP(b) SERF_BUCKET_CHECK((b), mmap)
+
+SERF_DECLARE(serf_bucket_t *) serf_bucket_mmap_create(
+    apr_mmap_t *mmap,
+    serf_bucket_alloc_t *allocator);
+
+
+/* ==================================================================== */
+
+
+/* ### do we need a PIPE bucket type? they are simple apr_file_t objects */
 
 
 #ifdef __cplusplus
