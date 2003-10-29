74869e6bc31cadafcd006a38d76b3b0373c10ad1
Greg Stein <gstein@apache.org>
2003-10-29 00:03:21 +0000

    The connection and context stuff are very, very tied, so let's put them into one source file. There will be too many hoops to keep the concepts in separate files.
    * serf/branches/gen2/connection.c: removed
    
    * serf/branches/gen2/context.c:
      (MAX_CONN): max number of connections a context can handle. note that
        this is a pretty broken concept passed off to us by APR. need to
        investigate further.
      (struct serf_context_t): filled out a bit
      (serf_connection_process): new, private function to process data in/out,
        and other conditions, on a connection.
      (serf_context_create): flesh out a bit
      (serf_context_run): sketch in some initial code
      (serf_context_add_connection, serf_context_remove_connection): new,
        internal functions to add/remove connections from a context. still
        somewhat sketchy, as we want to put these into ctx->conns, too.
      (serf_connection_create): moved from connection.c. filled out a bit.
      (serf_connection_request_create, serf_connection_request_cancel): moved
        from connection.c.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/commons/serf/branches/gen2@56 13f79535-47bb-0310-9956-ffa450edef68

--- context.c
+++ context.c
@@ -1,7 +1,7 @@
 /* ====================================================================
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * Copyright (c) 2003 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -53,19 +53,138 @@
 #include "serf.h"
 
 
+/* ### what the hell? why does the APR interface have a "size" ??
+   ### the implication is that, if we bust this limit, we'd need to
+   ### stop, rebuild a pollset, and repopulate it. what suckage.  */
+#define MAX_CONN 16
+
 struct serf_context_t {
-    int foo;
+    /* the pool used for self and for other allocations */
+    apr_pool_t *pool;
+
+    /* the set of connections to poll */
+    apr_pollset_t *pollset;
+
+    /* the list of active connections */
+    apr_array_header_t *conns;
 };
+
+
+static apr_status_t serf_connection_process(serf_connection_t *conn,
+                                            apr_int16_t events)
+{
+    return APR_SUCCESS;
+}
+
 
 
 SERF_DECLARE(serf_context_t *) serf_context_create(apr_pool_t *pool)
 {
-    return apr_pcalloc(pool, sizeof(serf_context_t));
+    serf_context_t *ctx = apr_pcalloc(pool, sizeof(*ctx));
+
+    ctx->pool = pool;
+
+    /* build the pollset with a (default) number of connections */
+    (void) apr_pollset_create(&ctx->pollset, MAX_CONN, pool, 0);
+
+    /* default to a single connection since that is the typical case */
+    ctx->conns = apr_array_make(pool, 1, sizeof(serf_connection_t *));
+
+    return ctx;
 }
 
 SERF_DECLARE(apr_status_t) serf_context_run(serf_context_t *ctx,
                                             apr_short_interval_time_t duration,
                                             apr_pool_t *pool)
+{
+    apr_status_t status;
+    apr_int32_t num;
+    const apr_pollfd_t *desc;
+
+    status = apr_pollset_poll(ctx->pollset, duration, &num, &desc);
+    if (status) {
+        /* ### do we still need to dispatch stuff here?
+           ### look at the potential return codes. map to our defined
+           ### return values? ...
+        */
+        return status;
+    }
+
+    while (num--) {
+        serf_connection_t *conn = desc->client_data;
+
+        status = serf_connection_process(conn, desc++->rtnevents);
+        if (status) {
+            /* ### what else to do? */
+            return status;
+        }
+    }
+
+    return APR_SUCCESS;
+}
+
+
+static apr_status_t serf_context_add_connection(serf_context_t *ctx,
+                                                serf_connection_t *conn)
+{
+    apr_pollfd_t desc = { 0 };
+
+    desc.desc_type = APR_POLL_SOCKET;
+    desc.reqevents = APR_POLLIN | APR_POLLOUT | APR_POLLERR | APR_POLLHUP;
+    desc.desc.s = conn->skt;
+    desc.client_data = conn;
+
+    return apr_pollset_add(ctx->pollset, &desc);
+}
+
+static apr_status_t serf_context_remove_connection(serf_context_t *ctx,
+                                                   serf_connection_t *conn)
+{
+    apr_pollfd_t desc = { 0 };
+
+    desc.desc_type = APR_POLL_SOCKET;
+    desc.desc.s = conn->skt;
+
+    return apr_pollset_remove(ctx->pollset, &desc);
+}
+
+SERF_DECLARE(serf_connection_t *) serf_connection_create(
+    serf_context_t *ctx,
+    apr_sockaddr_t *address,
+    serf_response_acceptor_t acceptor,
+    void *acceptor_baton,
+    serf_connection_closed_t closed,
+    void *closed_baton,
+    apr_pool_t *pool)
+{
+    serf_connection_t *conn = apr_pcalloc(pool, sizeof(*conn));
+
+    conn->ctx = ctx;
+    conn->pool = pool;
+    conn->acceptor = acceptor;
+    conn->acceptor_baton = acceptor_baton;
+    conn->closed = closed;
+    conn->closed_baton = closed_baton;
+
+    return conn;
+}
+
+
+SERF_DECLARE(apr_status_t) serf_connection_request_create(
+    serf_connection_t *conn,
+    serf_bucket_t *request,
+    serf_response_handler_t handler,
+    void *handler_baton,
+    serf_bucket_alloc_t *allocator,
+    apr_pool_t *pool)
+{
+    return APR_ENOTIMPL;
+}
+
+
+SERF_DECLARE(apr_status_t) serf_connection_request_cancel(
+    serf_connection_t *conn,
+    serf_bucket_t *request)
 {
     return APR_ENOTIMPL;
 }
