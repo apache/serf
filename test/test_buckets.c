ead389e3d9e67d4e72922dc8f46049e9b5a26b72
Ivan Zhakov <ivan@apache.org>
2007-10-11 10:02:22 +0000

    Simple tests for response bucket.
    * test/test_buckets.c
      (read_and_check_bucket): Helper for reading and checking buckets.
      (test_response_bucket_read): New test for HTTP/1.0 response.
      (test_response_bucket_chunked_read): New test for chunked HTTP/1.1 response.
      (test_buckets): Add new tests.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1698676 13f79535-47bb-0310-9956-ffa450edef68

--- test/test_buckets.c
+++ test/test_buckets.c
@@ -57,11 +57,86 @@ static void test_simple_bucket_readline(CuTest *tc)
     CuAssert(tc, data, strncmp("line2", data, len) == 0);
 }
 
+/* Reads bucket until EOF found and compares read data with zero terminated
+   string expected. Report all failures using CuTest. */
+static void read_and_check_bucket(CuTest *tc, serf_bucket_t *bkt,
+                                  const char *expected)
+{
+    apr_status_t status;
+    do
+    {
+        const char *data;
+        apr_size_t len;
+
+        status = serf_bucket_read(bkt, SERF_READ_ALL_AVAIL, &data, &len);
+        CuAssert(tc, "Got error during bucket reading.",
+                 !SERF_BUCKET_READ_ERROR(status));
+        CuAssert(tc, "Read more data than expected.",
+                 strlen(expected) >= len);
+        CuAssert(tc, "Read data is not equal to expected.",
+                 strncmp(expected, data, len) == 0);
+
+        expected += len;
+    } while(!APR_STATUS_IS_EOF(status));
+
+    CuAssert(tc, "Read less data than expected.", strlen(expected) == 0);
+}
+
+static void test_response_bucket_read(CuTest *tc)
+{
+    serf_bucket_alloc_t *alloc = serf_bucket_allocator_create(test_pool, NULL, NULL);
+    serf_bucket_t *bkt, *tmp;
+
+    tmp = SERF_BUCKET_SIMPLE_STRING(
+        "HTTP/1.1 200 OK" CRLF
+        "Content-Length: 7" CRLF
+        CRLF
+        "abc1234",
+        alloc);
+
+    bkt = serf_bucket_response_create(tmp, alloc);
+
+    /* Read all bucket and check it content. */
+    read_and_check_bucket(tc, bkt, "abc1234");
+}
+
+static void test_response_bucket_chunked_read(CuTest *tc)
+{
+    serf_bucket_alloc_t *alloc = serf_bucket_allocator_create(test_pool, NULL, NULL);
+    serf_bucket_t *bkt, *tmp, *hdrs;
+
+    tmp = SERF_BUCKET_SIMPLE_STRING(
+        "HTTP/1.1 200 OK" CRLF
+        "Transfer-Encoding: chunked" CRLF
+        CRLF
+        "3" CRLF
+        "abc" CRLF
+        "4" CRLF
+        "1234" CRLF
+        "0" CRLF
+        "Footer: value" CRLF
+        CRLF,
+        alloc);
+
+    bkt = serf_bucket_response_create(tmp, alloc);
+
+    /* Read all bucket and check it content. */
+    read_and_check_bucket(tc, bkt, "abc1234");
+
+    hdrs = serf_bucket_response_get_headers(bkt);
+    CuAssertTrue(tc, hdrs != NULL);
+
+    /* Check that trailing headers parsed correctly. */
+    CuAssertStrEquals(tc, "value", serf_bucket_headers_get(hdrs, "Footer"));
+}
+
 CuSuite *test_buckets(void)
 {
     CuSuite *suite = CuSuiteNew();
 
     SUITE_ADD_TEST(suite, test_simple_bucket_readline);
+    SUITE_ADD_TEST(suite, test_response_bucket_read);
+    SUITE_ADD_TEST(suite, test_response_bucket_chunked_read);
 
     return suite;
 }
