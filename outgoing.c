6434850efb09b1f9dea4d7f774bc78310f3d2cda
Justin Erenkrantz <jerenkrantz@apache.org>
2010-08-21 05:11:57 +0000

    Allow older Serf programs which don't use the new authn framework to still handle authn without forcing them to switch to the new framework.
    This permits Subversion 1.6.x-era ra_serf clients to authn properly.
    
    * outgoing.c
      (handle_response): Make the call to serf__handle_auth_response conditional
      on having a credential callback registered.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1698938 13f79535-47bb-0310-9956-ffa450edef68

--- outgoing.c
+++ outgoing.c
@@ -731,21 +731,29 @@ static apr_status_t handle_response(serf_request_t *request,
     apr_status_t status;
     int consumed_response = 0;
 
-    status = serf__handle_auth_response(&consumed_response,
-                                        request,
-                                        request->resp_bkt,
-                                        request->handler_baton,
-                                        pool);
+    /* Only enable the new authentication framework if the program has
+     * registered an authentication credential callback.
+     *
+     * This permits older Serf apps to still handle authentication
+     * themselves by not registering credential callbacks.
+     */
+    if (request->conn->ctx->cred_cb) {
+      status = serf__handle_auth_response(&consumed_response,
+                                          request,
+                                          request->resp_bkt,
+                                          request->handler_baton,
+                                          pool);
 
-    /* If there was an error reading the response (maybe there wasn't
-       enough data available), don't bother passing the response to the
-       application.
+      /* If there was an error reading the response (maybe there wasn't
+         enough data available), don't bother passing the response to the
+         application.
 
-       If the authentication was tried, but failed, pass the response
-       to the application, maybe it can do better. */
-    if (APR_STATUS_IS_EOF(status) ||
-        APR_STATUS_IS_EAGAIN(status)) {
-        return status;
+         If the authentication was tried, but failed, pass the response
+         to the application, maybe it can do better. */
+      if (APR_STATUS_IS_EOF(status) ||
+          APR_STATUS_IS_EAGAIN(status)) {
+          return status;
+      }
     }
 
     if (!consumed_response) {
