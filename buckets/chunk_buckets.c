91b25646379942ed582a1d3af38a6cba607e45ff
Justin Erenkrantz <jerenkrantz@apache.org>
2011-05-25 02:40:28 +0000

    Based on traffic analysis of ra_serf patterns, bump the chunk iovec size to 64 so we have a better chance of grabbing everything we want in one iovec.
    This cuts the TCP packet count on Subversion's basic benchmark tests from ~17k
    packets down to ~15k.
    
    * buckets/chunk_buckets.c
      (create_chunk): Bump from 32 to 64.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1699008 13f79535-47bb-0310-9956-ffa450edef68

--- buckets/chunk_buckets.c
+++ buckets/chunk_buckets.c
@@ -57,7 +57,7 @@ static apr_status_t create_chunk(serf_bucket_t *bucket)
     serf_bucket_t *simple_bkt;
     apr_size_t chunk_len;
     apr_size_t stream_len;
-    struct iovec vecs[34]; /* 32 + chunk trailer + EOF trailer = 34 */
+    struct iovec vecs[66]; /* 64 + chunk trailer + EOF trailer = 66 */
     int vecs_read;
     int i;
 
@@ -67,7 +67,7 @@ static apr_status_t create_chunk(serf_bucket_t *bucket)
 
     ctx->last_status =
         serf_bucket_read_iovec(ctx->stream, SERF_READ_ALL_AVAIL,
-                               32, vecs, &vecs_read);
+                               64, vecs, &vecs_read);
 
     if (SERF_BUCKET_READ_ERROR(ctx->last_status)) {
         /* Uh-oh. */
