83bb46958a2044c7aa1ec08455e7bf9d5ca8da01
Greg Stein <gstein@apache.org>
2011-06-21 03:09:44 +0000

    We're going to call the next release 1.0.0, and keep it alive for the duration of the Apache Subversion 1.7.x lifecycle.
    * serf.h:
      (SERF_MAJOR_VERSION, SERF_MINOR_VERSION): set to pending release values
    
    
    git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1699030 13f79535-47bb-0310-9956-ffa450edef68

--- serf.h
+++ serf.h
@@ -1053,8 +1053,8 @@ void serf_debug__bucket_alloc_check(
     serf_bucket_alloc_t *allocator);
 
 /* Version info */
-#define SERF_MAJOR_VERSION 0
-#define SERF_MINOR_VERSION 8
+#define SERF_MAJOR_VERSION 1
+#define SERF_MINOR_VERSION 0
 #define SERF_PATCH_VERSION 0
 
 /* Version number string */
