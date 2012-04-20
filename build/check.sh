0df0f8bf80fbecf69fbbb9abb5de956a3d11af90
Greg Stein <gstein@apache.org>
2012-04-20 21:29:34 +0000

    Remove posix-ism. The 'if' statement does not have '!'
    * build/check.sh: rejigger 'if ! ...' statements
    
    
    git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1699145 13f79535-47bb-0310-9956-ffa450edef68

--- build/check.sh
+++ build/check.sh
@@ -11,14 +11,14 @@ fi
 
 for case in "${TESTDIR}/testcases/"*.response; do
   echo "== Testing ${case} =="
-  if ! "${TESTDIR}/serf_response" "${case}"; then
+  if "${TESTDIR}/serf_response" "${case}"; then :; else
     echo "ERROR: test case failed"
     exit 1
   fi
 done
 
 # Run the CuTest-based tests
-if ! "${TESTDIR}/test_all"; then
+if "${TESTDIR}/test_all"; then :; else
   echo "ERROR: some test(s) failed in test_all"
   exit 1
 fi
