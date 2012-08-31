e65b7966f794ab5fa6511c35183c25b92af7d09d
Lieven Govaerts <lgo@apache.org>
2012-08-31 20:49:23 +0000

    Replace APR_EGENERAL error with two SSL specific error codes.
    * serf.h: New error codes SERF_ERROR_SSL_CERT_FAILED and
        SERF_ERROR_SSL_COMM_FAILED
    
    * buckets/ssl_buckets.c
      (*): Use the two new codes instead of APR_EGENERAL.
    
    * context.c
      (serf_error_string): give a description for the new error codes.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1699169 13f79535-47bb-0310-9956-ffa450edef68

--- serf.h
+++ serf.h
@@ -83,6 +83,12 @@ typedef struct serf_request_t serf_request_t;
 #define SERF_ERROR_BAD_HTTP_RESPONSE (APR_OS_START_USERERR + \
                                              SERF_ERROR_RANGE + 5)
 
+/* SSL certificates related errors */
+#define SERF_ERROR_SSL_CERT_FAILED (APR_OS_START_USERERR + SERF_ERROR_RANGE + 70)
+
+/* SSL communications related errors */
+#define SERF_ERROR_SSL_COMM_FAILED (APR_OS_START_USERERR + SERF_ERROR_RANGE + 71)
+
 /* General authentication related errors */
 #define SERF_ERROR_AUTHN_FAILED (APR_OS_START_USERERR + SERF_ERROR_RANGE + 90)
 
