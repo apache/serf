8cc171c8f49120e3e0f5fe19ac5efa47e78d121c
Ivan Zhakov <ivan@apache.org>
2013-07-05 12:05:17 +0000

    Explicitly use ANSI version of SSPI.
    * auth/auth_spnego_sspi.c
      (serf__spnego_create_sec_context): Use AcquireCredentialsHandleA().
      (serf__spnego_init_sec_context): InitializeSecurityContextA().
    
    
    git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1699542 13f79535-47bb-0310-9956-ffa450edef68

--- auth/auth_spnego_sspi.c
+++ auth/auth_spnego_sspi.c
@@ -138,7 +138,7 @@ serf__spnego_create_sec_context(serf__spnego_context_t **ctx_p,
     else
         sspi_package = "NTLM";
 
-    sspi_status = AcquireCredentialsHandle(
+    sspi_status = AcquireCredentialsHandleA(
         NULL, sspi_package, SECPKG_CRED_OUTBOUND,
         NULL, NULL, NULL, NULL,
         &ctx->sspi_credentials, NULL);
@@ -246,7 +246,7 @@ serf__spnego_init_sec_context(serf__spnego_context_t *ctx,
     sspi_out_buffer_desc.pBuffers = &sspi_out_buffer;
     sspi_out_buffer_desc.ulVersion = SECBUFFER_VERSION;
 
-    status = InitializeSecurityContext(
+    status = InitializeSecurityContextA(
         &ctx->sspi_credentials,
         ctx->initalized ? &ctx->sspi_context : NULL,
         ctx->target_name,
