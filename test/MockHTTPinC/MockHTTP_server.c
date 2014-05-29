13256becc0ac8d81f89cb422ea6e6fd44e9ca01e
Lieven Govaerts <lgo@apache.org>
2014-05-29 10:43:00 +0000

    In the test suite, update the MockHTTPinC library to the latest version.
    * test/MockHTTPinC/MockHTTP_server.c
      (processServer): Follow up to r2362: break out of the loop when no more data
          can be written to the network. Reset logging flags.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1699896 13f79535-47bb-0310-9956-ffa450edef68

--- test/MockHTTPinC/MockHTTP_server.c
+++ test/MockHTTPinC/MockHTTP_server.c
@@ -1095,14 +1095,14 @@ static apr_status_t processServer(mhServCtx_t *ctx, _mhClientCtx_t *cctx,
             resp = cctx->currResp ? cctx->currResp :
                             *(mhResponse_t **)apr_array_pop(cctx->respQueue);
             if (resp) {
-                _mhLog(1, cctx->skt, "Sending response to client.\n");
+                _mhLog(MH_VERBOSE, cctx->skt, "Sending response to client.\n");
 
                 status = writeResponse(cctx, resp);
                 if (status == APR_EOF) {
                     cctx->currResp = NULL;
                     ctx->mh->verifyStats->requestsResponded++;
                     if (resp->closeConn) {
-                        _mhLog(1, cctx->skt,
+                        _mhLog(MH_VERBOSE, cctx->skt,
                                "Actively closing connection.\n");
                         return APR_EOF;
                     }
@@ -1110,6 +1110,7 @@ static apr_status_t processServer(mhServCtx_t *ctx, _mhClientCtx_t *cctx,
                 } else {
                     cctx->currResp = resp;
                     status = APR_EAGAIN;
+                    break;
                 }
             } else {
                 return APR_EGENERAL;
