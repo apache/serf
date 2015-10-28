7828c73962fc5ae8c1b64d4ef7e804a8c199bcfa
Bert Huijben <rhuijben@apache.org>
2015-10-28 09:53:30 +0000

    * buckets/hpack_buckets.c   (header): Fix indentation.
    git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1710973 13f79535-47bb-0310-9956-ffa450edef68

--- buckets/hpack_buckets.c
+++ buckets/hpack_buckets.c
@@ -1,22 +1,22 @@
 /* ====================================================================
-*    Licensed to the Apache Software Foundation (ASF) under one
-*    or more contributor license agreements.  See the NOTICE file
-*    distributed with this work for additional information
-*    regarding copyright ownership.  The ASF licenses this file
-*    to you under the Apache License, Version 2.0 (the
-*    "License"); you may not use this file except in compliance
-*    with the License.  You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-*    Unless required by applicable law or agreed to in writing,
-*    software distributed under the License is distributed on an
-*    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-*    KIND, either express or implied.  See the License for the
-*    specific language governing permissions and limitations
-*    under the License.
-* ====================================================================
-*/
+ *    Licensed to the Apache Software Foundation (ASF) under one
+ *    or more contributor license agreements.  See the NOTICE file
+ *    distributed with this work for additional information
+ *    regarding copyright ownership.  The ASF licenses this file
+ *    to you under the Apache License, Version 2.0 (the
+ *    "License"); you may not use this file except in compliance
+ *    with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an
+ *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, either express or implied.  See the License for the
+ *    specific language governing permissions and limitations
+ *    under the License.
+ * ====================================================================
+ */
 
 #include <stdlib.h>
 
@@ -462,11 +462,16 @@ hpack_copy_from_headers(void *baton,
                         const char *value)
 {
   serf_bucket_t *hpack = baton;
+  apr_size_t key_sz = strlen(key);
 
   /* TODO: Others? */
-  if (!strcasecmp(key, "Host")
-      || !strcasecmp(key, "Connection")
-      || !strncasecmp(key, "Connection-", 11))
+  if ((key_sz == 4 && !strcasecmp(key, "Host"))
+      || (key_sz == 7 && !strcasecmp(key, "Upgrade"))
+      || (key_sz == 10 && !strcasecmp(key, "Keep-Alive"))
+      || (key_sz == 10 && !strcasecmp(key, "Connection"))
+      || (key_sz > 11 && !strncasecmp(key, "Connection-", 11))
+      || (key_sz == 16 && !strcasecmp(key, "Proxy-Connection"))
+      || (key_sz == 17 && !strcasecmp(key, "Transfer-Encoding")))
     {
       return APR_SUCCESS;
     }
