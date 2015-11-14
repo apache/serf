ba352cfe7a57e1a6d28314cc321101b4615434cf
Bert Huijben <rhuijben@apache.org>
2015-11-14 09:36:08 +0000

    Redefine the event bucket as a wrapping bucket, to remove dependencies on implementation details on the 'aggregation' stream around the event bucket.
    Calculate total number of bytes read to increase event value.
    
    * buckets/event_buckets.c
      (event_context_t): Extend state a bit.
      (serf__bucket_event_create): Add start event and stream arguments.
      (serf_event_read): Implement wrapping and early destroy of stream.
      (serf_event_readline): Remove function. The default implementation will
        work good enough for writing stream scenarios for now.
      (serf_event_read_iovec,
       serf_event_peek,
       serf_event_get_remaining): Implement wrapping and early destroy of stream.
      (serf_bucket_type__event): Update declaration.
    
    * outgoing.c
      (request_writing_done,
       request_writing_finished): Add argument.
      (write_to_connection): Update registration.
    
    * serf_private.h
      (serf_bucket_event_callback_t): Add bytes_read argument.
      (serf__bucket_event_create): Add stream and start_cb arguments.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1714297 13f79535-47bb-0310-9956-ffa450edef68

--- buckets/event_buckets.c
+++ buckets/event_buckets.c
@@ -25,21 +25,31 @@
 
 typedef struct event_context_t
 {
+    apr_uint64_t bytes_read;
+    serf_bucket_t *stream;
     void *baton;
+    serf_bucket_event_callback_t start_cb;
     serf_bucket_event_callback_t eof_cb;
     serf_bucket_event_callback_t destroy_cb;
+    bool at_eof;
 } event_context_t;
 
 serf_bucket_t *serf__bucket_event_create(
-                          void *baton,
-                          serf_bucket_event_callback_t eof_cb,
-                          serf_bucket_event_callback_t destroy_cb,
-                          serf_bucket_alloc_t *allocator)
+                        serf_bucket_t *stream,
+                        void *baton,
+                        serf_bucket_event_callback_t start_cb,
+                        serf_bucket_event_callback_t eof_cb,
+                        serf_bucket_event_callback_t destroy_cb,
+                        serf_bucket_alloc_t *allocator)
 {
     event_context_t *ctx = serf_bucket_mem_alloc(allocator, sizeof(*ctx));
+    ctx->bytes_read = 0;
+    ctx->stream = stream;
     ctx->baton = baton;
+    ctx->start_cb = start_cb;
     ctx->eof_cb = eof_cb;
     ctx->destroy_cb = destroy_cb;
+    ctx->at_eof = false;
 
     return serf_bucket_create(&serf_bucket_type__event, allocator, ctx);
 }
@@ -50,30 +60,45 @@ static apr_status_t serf_event_read(serf_bucket_t *bucket,
                                     apr_size_t *len)
 {
     event_context_t *ctx = bucket->data;
-    apr_status_t status = APR_EOF;
-    *data = NULL;
-    *len = 0;
+    apr_status_t status;
 
-    if (ctx->eof_cb)
-        status = ctx->eof_cb(ctx->baton);
+    if (ctx->start_cb) {
+        status = ctx->start_cb(ctx->baton, ctx->bytes_read);
+        ctx->start_cb = NULL;
 
-    return status ? status : APR_EOF;
-}
+        if (SERF_BUCKET_READ_ERROR(status))
+            return status;
+    }
 
-static apr_status_t serf_event_readline(serf_bucket_t *bucket,
-                                        int acceptable, int *found,
-                                        const char **data, apr_size_t *len)
-{
-    event_context_t *ctx = bucket->data;
-    apr_status_t status = APR_EOF;
-    *found = 0;
-    *data = NULL;
-    *len = 0;
+    if (ctx->stream && !ctx->at_eof) {
+        status = serf_bucket_read(ctx->stream, requested, data, len);
+    }
+    else {
+        status = APR_EOF;
+        *data = NULL;
+        *len = 0;
 
-    if (ctx->eof_cb)
-        status = ctx->eof_cb(ctx->baton);
+        if (ctx->at_eof && ctx->stream) {
+            serf_bucket_destroy(ctx->stream);
+            ctx->stream = NULL;
+        }
+    }
 
-    return status ? status : APR_EOF;
+    if (!SERF_BUCKET_READ_ERROR(status)) {
+        ctx->bytes_read += *len;
+
+        if (APR_STATUS_IS_EOF(status)) {
+            ctx->at_eof = true;
+
+            if (ctx->eof_cb) {
+                status = ctx->eof_cb(ctx->baton, ctx->bytes_read);
+                ctx->eof_cb = NULL;
+                status = SERF_BUCKET_READ_ERROR(status) ? status : APR_EOF;
+            }
+        }
+    }
+
+    return status;
 }
 
 static apr_status_t serf_event_read_iovec(serf_bucket_t *bucket,
@@ -83,13 +108,48 @@ static apr_status_t serf_event_read_iovec(serf_bucket_t *bucket,
                                           int *vecs_used)
 {
     event_context_t *ctx = bucket->data;
-    apr_status_t status = APR_EOF;
-    *vecs_used = 0;
+    apr_status_t status;
 
-    if (ctx->eof_cb)
-        status = ctx->eof_cb(ctx->baton);
+    if (ctx->start_cb) {
+        status = ctx->start_cb(ctx->baton, ctx->bytes_read);
+        ctx->start_cb = NULL;
 
-    return status ? status : APR_EOF;
+        if (SERF_BUCKET_READ_ERROR(status))
+            return status;
+    }
+
+    if (ctx->stream && !ctx->at_eof) {
+        status = serf_bucket_read_iovec(ctx->stream, requested, vecs_size,
+                                        vecs, vecs_used);
+    }
+    else {
+        status = APR_EOF;
+        *vecs_used = 0;
+
+        if (ctx->at_eof && ctx->stream) {
+            serf_bucket_destroy(ctx->stream);
+            ctx->stream = NULL;
+        }
+    }
+
+    if (!SERF_BUCKET_READ_ERROR(status)) {
+        int i;
+
+        for (i = 0; i < *vecs_used; i++)
+            ctx->bytes_read += vecs[i].iov_len;
+
+        if (APR_STATUS_IS_EOF(status)) {
+            ctx->at_eof = true;
+
+            if (ctx->eof_cb) {
+                status = ctx->eof_cb(ctx->baton, ctx->bytes_read);
+                ctx->eof_cb = NULL;
+                status = SERF_BUCKET_READ_ERROR(status) ? status : APR_EOF;
+            }
+        }
+    }
+
+    return status;
 }
 
 static apr_status_t serf_event_peek(serf_bucket_t *bucket,
@@ -97,30 +157,57 @@ static apr_status_t serf_event_peek(serf_bucket_t *bucket,
                                     apr_size_t *len)
 {
     event_context_t *ctx = bucket->data;
-    apr_status_t status = APR_EOF;
-    *data = NULL;
-    *len = 0;
+    apr_status_t status;
 
-    if (ctx->eof_cb)
-        status = ctx->eof_cb(ctx->baton);
+    if (ctx->start_cb) {
+        status = ctx->start_cb(ctx->baton, ctx->bytes_read);
+        ctx->start_cb = NULL;
 
-    if (APR_STATUS_IS_EAGAIN(status))
-        return APR_SUCCESS;
-    else
-        return status ? status : APR_EOF;
+        if (SERF_BUCKET_READ_ERROR(status))
+            return status;
+    }
+
+    if (ctx->stream && !ctx->at_eof) {
+        status = serf_bucket_peek(ctx->stream, data, len);
+    }
+    else {
+        status = APR_EOF;
+        *len = 0;
+
+        if (ctx->at_eof && ctx->stream) {
+            serf_bucket_destroy(ctx->stream);
+            ctx->stream = NULL;
+        }
+    }
+
+    return status;
 }
 
 static apr_uint64_t serf_event_get_remaining(serf_bucket_t *bucket)
 {
-    return 0;
+    event_context_t *ctx = bucket->data;
+
+    if (ctx->stream && !ctx->at_eof) {
+        return serf_bucket_get_remaining(ctx->stream);
+    }
+    else {
+        if (ctx->at_eof && ctx->stream) {
+            serf_bucket_destroy(ctx->stream);
+            ctx->stream = NULL;
+        }
+        return 0;
+    }
 }
 
 static void serf_event_destroy(serf_bucket_t *bucket)
 {
     event_context_t *ctx = bucket->data;
 
+    if (ctx->stream)
+        serf_bucket_destroy(ctx->stream);
+
     if (ctx->destroy_cb)
-        (void)ctx->destroy_cb(ctx->baton);
+        (void)ctx->destroy_cb(ctx->baton, ctx->bytes_read);
 
     serf_default_destroy_and_data(bucket);
 }
@@ -128,7 +215,7 @@ static void serf_event_destroy(serf_bucket_t *bucket)
 const serf_bucket_type_t serf_bucket_type__event = {
     "EVENT",
     serf_event_read,
-    serf_event_readline,
+    serf_default_readline,
     serf_event_read_iovec,
     serf_default_read_for_sendfile,
     serf_buckets_are_v2,
