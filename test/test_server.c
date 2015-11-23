6494d9ff92b959140250fa40b15b5183e4a0d3ef
Bert Huijben <rhuijben@apache.org>
2015-11-23 10:55:54 +0000

    * test/test_server.c   (test_server): Temporary disable the http2 server tests to see if this     changes behavior on the mac bots.
    git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1715758 13f79535-47bb-0310-9956-ffa450edef68

--- test/test_server.c
+++ test/test_server.c
@@ -336,10 +336,10 @@ CuSuite *test_server(void)
     CuSuiteSetSetupTeardownCallbacks(suite, test_setup, test_teardown);
 
     SUITE_ADD_TEST(suite, test_listen_http);
-    SUITE_ADD_TEST(suite, test_listen_http2);
+    /*SUITE_ADD_TEST(suite, test_listen_http2);*/
 
     SUITE_ADD_TEST(suite, test_listen_auth_http);
-    SUITE_ADD_TEST(suite, test_listen_auth_http2);
+    /*SUITE_ADD_TEST(suite, test_listen_auth_http2);*/
 
     return suite;
 }
