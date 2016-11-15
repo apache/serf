7ef36595e2ce8143331210147e9afb156bfe6e24
Branko Čibej <brane@apache.org>
2016-11-15 12:34:08 +0000

    [Reverted in r1769841.]
    Make sure that serf_connection_create() initializes the config store.
    
    * serf_private.h (serf__config_store_create_conn_config):
       Introduce an optional pool parameter for temporary allocations that,
       when provided, assures that this function will not return an error.
       Update the docstring.
    
    * src/config_store.c (serf__config_store_create_conn_config):
       Only create a temporary pool (and, hence, possibly return an error)
       if an optional pool was not provided.
    
    * src/outgoing.c
      (create_connection): New.
       Factor out most of the contents of serf_connection_create().
      (serf_connection_create): Call create_connection(), then initialize
       the connection's config store.
      (serf_connection_create2): Call create_connection() instead of
       serf_connection_create(). Update the invocation of
       serf__config_store_create_conn_config().
    
    * test/test_internal.c
      (test_config_store_per_connection_different_host,
       test_config_store_per_connection_same_host,
       test_config_store_remove_objects):
       Update all invocations of serf__config_store_create_conn_config().
    
    
    git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1769799 13f79535-47bb-0310-9956-ffa450edef68

--- src/config_store.c
+++ src/config_store.c
@@ -236,21 +236,27 @@ apr_status_t serf__config_store_create_ctx_config(serf_context_t *ctx,
 }
 
 apr_status_t serf__config_store_create_conn_config(serf_connection_t *conn,
-                                                   serf_config_t **config)
+                                                   serf_config_t **config,
+                                                   apr_pool_t *opt_pool)
 {
     serf__config_store_t *config_store = &conn->ctx->config_store;
     const char *host_key, *conn_key;
     serf__config_hdr_t *per_conn, *per_host;
     apr_pool_t *tmp_pool;
-    apr_status_t status;
 
     serf_config_t *cfg = apr_pcalloc(conn->pool, sizeof(serf_config_t));
     cfg->ctx_pool = config_store->pool;
     cfg->allocator = config_store->allocator;
     cfg->per_context = config_store->global_per_context;
 
-    if ((status = apr_pool_create(&tmp_pool, cfg->ctx_pool)) != APR_SUCCESS)
-        return status;
+    if (!opt_pool) {
+        apr_status_t status = apr_pool_create(&tmp_pool, cfg->ctx_pool);
+        if (status != APR_SUCCESS)
+            return status;
+    }
+    else {
+        tmp_pool = opt_pool;
+    }
 
     /* Find the config values for this connection, create empty structure
         if needed */
@@ -279,7 +285,8 @@ apr_status_t serf__config_store_create_conn_config(serf_connection_t *conn,
     }
     cfg->per_host = per_host;
 
-    apr_pool_destroy(tmp_pool);
+    if (tmp_pool != opt_pool)
+        apr_pool_destroy(tmp_pool);
 
     *config = cfg;
 
