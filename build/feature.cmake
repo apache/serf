80bd46f610ca93832d0052b2ea7d901dec6cd9c8
Branko Čibej <brane@apache.org>
2018-06-23 16:26:58 +0000

    Start working on a CMake-based build to eventually replace SCons.
    * CMakeLists.txt,
      build/feature.cmake, build/version.cmake,
      build/FindApr.cmake, build/FindApu.cmake: New files.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1834217 13f79535-47bb-0310-9956-ffa450edef68

---
+++ build/feature.cmake
@@ -0,0 +1,87 @@
+# ===================================================================
+#   Licensed to the Apache Software Foundation (ASF) under one
+#   or more contributor license agreements.  See the NOTICE file
+#   distributed with this work for additional information
+#   regarding copyright ownership.  The ASF licenses this file
+#   to you under the Apache License, Version 2.0 (the
+#   "License"); you may not use this file except in compliance
+#   with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing,
+#   software distributed under the License is distributed on an
+#   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#   KIND, either express or implied.  See the License for the
+#   specific language governing permissions and limitations
+#   under the License.
+# ===================================================================
+
+include(CheckFunctionExists)
+include(CheckIncludeFile)
+include(CheckTypeSize)
+
+function(_CheckFunction var_ name_ libraries_)
+  set(CMAKE_REQUIRED_LIBRARIES "${libraries_}")
+  check_function_exists("${name_}" "serf_foundit_${name_}_")
+  if(${serf_foundit_${name_}_})
+    set("${var_}" TRUE PARENT_SCOPE)
+  else()
+    set("${var_}" FALSE PARENT_SCOPE)
+  endif()
+  unset(CMAKE_REQUIRED_LIBRARIES)
+endfunction(_CheckFunction)
+
+macro(CheckFunction name_ symbol_)
+  _CheckFunction("serf_feature_CheckFunction_${name}_" "${name_}" "${ARGN}")
+  if("${serf_feature_CheckFunction_${name}_}")
+    add_definitions("-D${symbol_}")
+  endif()
+endmacro(CheckFunction)
+
+macro(CheckNotFunction name_ symbol_)
+  _CheckFunction("serf_feature_CheckNotFunction_${name}_" "${name_}" "${ARGN}")
+  if(NOT "${serf_feature_CheckNotFunction_${name}_}")
+    add_definitions("-D${symbol_}")
+  endif()
+endmacro(CheckNotFunction)
+
+
+function(_CheckHeader var_ name_ includes_)
+  set(CMAKE_REQUIRED_INCLUDES "${includes_}")
+  check_include_file("${name_}" "serf_foundit_${name_}_")
+  if(${serf_foundit_${name_}_})
+    set("${var_}" TRUE PARENT_SCOPE)
+  else()
+    set("${var_}" FALSE PARENT_SCOPE)
+  endif()
+  unset(CMAKE_REQUIRED_INCLUDES)
+endfunction(_CheckHeader)
+
+macro(CheckHeader name_ symbol_)
+  _CheckHeader("serf_feature_CheckHeader_${name}_" "${name_}" "${ARGN}")
+  if("${serf_feature_CheckHeader_${name}_}")
+    add_definitions("-D${symbol_}")
+  endif()
+endmacro(CheckHeader)
+
+
+function(_CheckType var_ name_ header_ includes_)
+  set(CMAKE_REQUIRED_INCLUDES "${includes_}")
+  set(CMAKE_EXTRA_INCLUDE_FILES "${header_}")
+  check_type_size("${name_}" "serf_foundit_${name_}_")
+  if(${HAVE_serf_foundit_${name_}_})
+    set("${var_}" TRUE PARENT_SCOPE)
+  else()
+    set("${var_}" FALSE PARENT_SCOPE)
+  endif()
+  unset(CMAKE_REQUIRED_INCLUDES)
+  unset(CMAKE_EXTRA_INCLUDE_FILES)
+endfunction(_CheckType)
+
+macro(CheckType name_ header_ symbol_)
+  _CheckType("serf_feature_CheckType_${name}_" "${name_}" "${header_}" "${ARGN}")
+  if("${serf_feature_CheckType_${name}_}")
+    add_definitions("-D${symbol_}")
+  endif()
+endmacro(CheckType)
---
+++ build/version.cmake
@@ -0,0 +1,54 @@
+# ===================================================================
+#   Licensed to the Apache Software Foundation (ASF) under one
+#   or more contributor license agreements.  See the NOTICE file
+#   distributed with this work for additional information
+#   regarding copyright ownership.  The ASF licenses this file
+#   to you under the Apache License, Version 2.0 (the
+#   "License"); you may not use this file except in compliance
+#   with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing,
+#   software distributed under the License is distributed on an
+#   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#   KIND, either express or implied.  See the License for the
+#   specific language governing permissions and limitations
+#   under the License.
+# ===================================================================
+
+# Find the version number in serf.h so that we don't keep it in two places.
+
+set(SERF_HEADER "${CMAKE_SOURCE_DIR}/serf.h")
+
+unset(SERF_VERSION)
+unset(SERF_SOVERSION)
+unset(SERF_MAJOR_VERSION)
+unset(SERF_MINOR_VERSION)
+unset(SERF_PATCH_VERSION)
+
+file(STRINGS "${SERF_HEADER}" SERF_VERSION_BITS
+     REGEX "^ *# *define +SERF_[A-Z]*_VERSION +[0-9]+ *$")
+foreach(STR ${SERF_VERSION_BITS})
+  if(STR MATCHES "^ *# *define +SERF_MAJOR_VERSION +([0-9])+ *$")
+    string(REGEX REPLACE "^ *# *define +SERF_MAJOR_VERSION +([0-9])+ *$"
+           "\\1" SERF_MAJOR_VERSION ${STR})
+  endif()
+  if(STR MATCHES "^ *# *define +SERF_MINOR_VERSION +([0-9])+ *$")
+    string(REGEX REPLACE "^ *# *define +SERF_MINOR_VERSION +([0-9])+ *$"
+           "\\1" SERF_MINOR_VERSION ${STR})
+  endif()
+  if(STR MATCHES "^ *# *define +SERF_PATCH_VERSION +([0-9])+ *$")
+    string(REGEX REPLACE "^ *# *define +SERF_PATCH_VERSION +([0-9])+ *$"
+           "\\1" SERF_PATCH_VERSION ${STR})
+  endif()
+endforeach()
+
+if(NOT DEFINED SERF_MAJOR_VERSION
+   OR NOT DEFINED SERF_MINOR_VERSION
+   OR NOT DEFINED SERF_PATCH_VERSION)
+  message(FATAL_ERROR "Could not find the version number in '${SERF_HEADER}'")
+endif()
+
+set(SERF_VERSION "${SERF_MAJOR_VERSION}.${SERF_MINOR_VERSION}.${SERF_PATCH_VERSION}")
+set(SERF_SOVERSION "${SERF_MAJOR_VERSION}.${SERF_MINOR_VERSION}.0")
