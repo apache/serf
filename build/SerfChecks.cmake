9c0b17a2402eb28b1f09c41c35a21bea0d8d9325
Branko Čibej <brane@apache.org>
2018-06-25 06:23:33 +0000

    Make the CMake build more like the current SCons build.
    * CMakeLists.txt:
       Add build options with names like the SCons options.
       Add missing config checks and set compiler debug and warning flags.
    
    * test/CMakeLists.txt:
       Force C99 compilation mode if the top-level project forced C89 mode.
    
    * build/SerfChecks.cmake:
       Make the feature tests use a more strict environment.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1834279 13f79535-47bb-0310-9956-ffa450edef68

--- build/SerfChecks.cmake
+++ build/SerfChecks.cmake
@@ -22,7 +22,12 @@ include(CheckIncludeFile)
 include(CheckTypeSize)
 
 function(_CheckFunction var_ name_ libraries_)
-  set(CMAKE_REQUIRED_LIBRARIES "${libraries_}")
+  if(libraries_)
+    set(CMAKE_REQUIRED_LIBRARIES "${libraries_}")
+  else()
+    unset(CMAKE_REQUIRED_LIBRARIES)
+  endif()
+
   check_function_exists("${name_}" "serf_foundit_${name_}_")
   if(${serf_foundit_${name_}_})
     set("${var_}" TRUE PARENT_SCOPE)
@@ -48,7 +53,12 @@ endmacro(CheckNotFunction)
 
 
 function(_CheckHeader var_ name_ includes_)
-  set(CMAKE_REQUIRED_INCLUDES "${includes_}")
+  if(includes_)
+    set(CMAKE_REQUIRED_INCLUDES "${includes_}")
+  else()
+    unset(CMAKE_REQUIRED_INCLUDES)
+  endif()
+
   check_include_file("${name_}" "serf_foundit_${name_}_")
   if(${serf_foundit_${name_}_})
     set("${var_}" TRUE PARENT_SCOPE)
@@ -67,8 +77,18 @@ endmacro(CheckHeader)
 
 
 function(_CheckType var_ name_ header_ includes_)
-  set(CMAKE_REQUIRED_INCLUDES "${includes_}")
-  set(CMAKE_EXTRA_INCLUDE_FILES "${header_}")
+  if(includes_)
+    set(CMAKE_REQUIRED_INCLUDES "${includes_}")
+  else()
+    unset(CMAKE_REQUIRED_INCLUDES)
+  endif()
+
+  if(header_)
+    set(CMAKE_EXTRA_INCLUDE_FILES "${header_}")
+  else()
+    unset(CMAKE_EXTRA_INCLUDE_FILES)
+  endif()
+
   check_type_size("${name_}" "serf_foundit_${name_}_")
   if(${HAVE_serf_foundit_${name_}_})
     set("${var_}" TRUE PARENT_SCOPE)
