apache/serf

commit 045fd6bc8b0347cb193f74177f6848dba4d6c492

Fix non-source layout APR include paths on Windows.
APR builds on Windows (both CMake- and the old Makefile.win-based) do not
install include files in $PREFIX/include/apr-1 but just in $PREFIX/include.

* SConstruct: Omit the '.../apr-1' directory from APR and APR-Util include
   paths on Windows.

git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1833230 13f79535-47bb-0310-9956-ffa450edef68

SConstruct
@@ -356,7 +356,7 @@ if sys.platform == 'win32':
   env.Append(LIBS=[apr_libs, apu_libs])
   if not env.get('SOURCE_LAYOUT', None):
     env.Append(LIBPATH=['$APR/lib', '$APU/lib'],
-               CPPPATH=['$APR/include/apr-1', '$APU/include/apr-1'])
+               CPPPATH=['$APR/include', '$APU/include'])
   elif aprstatic:
     env.Append(LIBPATH=['$APR/LibR','$APU/LibR'],
                CPPPATH=['$APR/include', '$APU/include'])

commit 8e453ec766144f92d77a8da0ddab3983a8346b44

* SConstruct: Restore finding apr on the Windows buildbot. Broken since r1833230.
git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1866470 13f79535-47bb-0310-9956-ffa450edef68

SConstruct
@@ -390,7 +390,8 @@ if sys.platform == 'win32':
 
   if not env.get('SOURCE_LAYOUT', None):
     env.Append(LIBPATH=['$APR/lib', '$APU/lib'],
-               CPPPATH=['$APR/include', '$APU/include'])
+               CPPPATH=['$APR/include', '$APR/include/apr-1',
+                        '$APU/include', '$APU/include/apr-1'])
   elif aprstatic:
     env.Append(LIBPATH=['$APR/LibR','$APU/LibR'],
                CPPPATH=['$APR/include', '$APU/include'])