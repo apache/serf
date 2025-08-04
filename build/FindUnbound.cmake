fc79b45ddbfc1276a75487510684200b0d362e3a
Branko Čibej <brane@apache.org>
2025-08-04 09:15:23 +0000

    In the CMake build: Search for most dependencies with pkg-config first, and find_package if that fails.
    * CMakeLists.txt:
       - Use pkg-config to look for OpenSSL, zlib, Brotli, GSSAPI and Unbound.
       - Write the pkg-config file with dependencies as appropriate.
    * build/FindUnbound.cmake: Remove pkg-config related parts.
    * build/SerfFindPkgConfig.cmake: New.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/serf/trunk@1927605 13f79535-47bb-0310-9956-ffa450edef68

--- build/FindUnbound.cmake
+++ build/FindUnbound.cmake
@@ -43,13 +43,6 @@ cmake_minimum_required(VERSION 3.12)
 #   UNBOUND_INCLUDE_DIR  - Where to find unbound.h.
 #   UNBOUND_LIBRARY      - Linker switches to use with ld to link the library.
 #
-# ::
-#
-#   UNBOUND_PC_REQUIRES  - The name of the pkg-config module for libunbound;
-#                          empty if pkg-config was not used to find the library.
-#   UNBOUND_PC_LIBS      - The link libraries for pkg-config, if
-#                          UNBOUND_PC_REQUUIRES is not set.
-#
 # Hints
 # ^^^^^
 #
@@ -57,102 +50,54 @@ cmake_minimum_required(VERSION 3.12)
 
 include(FindPackageHandleStandardArgs)
 include(GNUInstallDirs)
-find_package(PkgConfig QUIET)
-
-# Save the PKG_CONFIG_PATH environment variable
-if(PKG_CONFIG_FOUND)
-  set(_pkg_config_path $ENV{PKG_CONFIG_PATH})
-endif()
 
 set(Unbound_FOUND FALSE)
 if(DEFINED Unbound_ROOT)
   # Search in the provided root path
   set(_root_search PATHS ${Unbound_ROOT} NO_DEFAULT_PATH)
-  list(APPEND _UNBOUND_SEARCHES _root_search)
-
-  # Set the PKG_CONFIG_PATH environment variable
-  if(PKG_CONFIG_FOUND)
-    find_path(_pcfile NAMES "libunbound.pc"
-              ${_root_search}
-              PATH_SUFFIXES
-              "lib/pkgconfig"
-              "${CMAKE_INSTALL_LIBDIR}/pkgconfig"
-              "share/pkgconfig"
-              "${CMAKE_INSTALL_DATAROOTDIR}/pkgconfig")
-    if(_pcfile AND EXISTS "${_pcfile}/libunbound.pc")
-      set(ENV{PKG_CONFIG_PATH} ${_pcfile})
-    endif()
-  endif()
+  list(APPEND _unbound_searches _root_search)
 endif()
 
 # Search in default paths
 set(_default_search)
-list(APPEND _UNBOUND_SEARCHES _default_search)
+list(APPEND _unbound_searches _default_search)
 
-# Try pkg-config first
-if(PKG_CONFIG_FOUND)
-  pkg_search_module(UNBOUND QUIET IMPORTED_TARGET libunbound)
-  if(UNBOUND_FOUND)
-    find_package_handle_standard_args(Unbound
-      REQUIRED_VARS UNBOUND_LINK_LIBRARIES UNBOUND_INCLUDEDIR
-      VERSION_VAR UNBOUND_VERSION)
-    if(Unbound_FOUND)
-      add_library(Unbound::Unbound ALIAS PkgConfig::UNBOUND)
-      set(UNBOUND_INCLUDE_DIR ${UNBOUND_INCLUDEDIR})
-      set(UNBOUND_LIBRARY ${UNBOUND_LINK_LIBRARIES})
-      set(UNBOUND_PC_REQUIRES libunbound)
-      set(UNBOUND_PC_LIBS)
-    endif()
-  endif()
-endif()
-
-if(NOT Unbound_FOUND)
-  # Try each search configuration
-  foreach(search ${_UNBOUND_SEARCHES})
-    find_path(UNBOUND_INCLUDE_DIR NAMES "unbound.h" ${${search}}
-              PATH_SUFFIXES "include" "${CMAKE_INSTALL_INCLUDEDIR}")
-    if(UNBOUND_INCLUDE_DIR AND EXISTS "${UNBOUND_INCLUDE_DIR}/unbound.h")
-      # Use the first successful search to find the libraries
-      if(NOT DEFINED libsearch)
-        set(libsearch ${search})
-      endif()
-    endif()
-  endforeach()
-
-  find_library(UNBOUND_LIBRARY NAMES "unbound" NAMES_PER_DIR ${${libsearch}}
-               PATH_SUFFIXES "lib" "${CMAKE_INSTALL_LIBDIR}")
-
-  # Extract the version number from the header
+# Try each search configuration
+foreach(search ${_unbound_searches})
+  find_path(UNBOUND_INCLUDE_DIR NAMES "unbound.h" ${${search}}
+            PATH_SUFFIXES "include" "${CMAKE_INSTALL_INCLUDEDIR}")
   if(UNBOUND_INCLUDE_DIR AND EXISTS "${UNBOUND_INCLUDE_DIR}/unbound.h")
-    file(STRINGS "${UNBOUND_INCLUDE_DIR}/unbound.h" _major
-      REGEX "^ *# *define +UNBOUND_VERSION_MAJOR +[0-9]+.*$")
-    file(STRINGS "${UNBOUND_INCLUDE_DIR}/unbound.h" _minor
-      REGEX "^ *# *define +UNBOUND_VERSION_MINOR +[0-9]+.*$")
-    file(STRINGS "${UNBOUND_INCLUDE_DIR}/unbound.h" _micro
-      REGEX "^ *# *define +UNBOUND_VERSION_MICRO +[0-9]+.*$")
-    string(REGEX REPLACE "^[^0-9]+([0-9]+).*$" "\\1" _major ${_major})
-    string(REGEX REPLACE "^[^0-9]+([0-9]+).*$" "\\1" _minor ${_minor})
-    string(REGEX REPLACE "^[^0-9]+([0-9]+).*$" "\\1" _micro ${_micro})
-    set(UNBOUND_VERSION "${_major}.${_minor}.${_micro}")
+    # Use the first successful search to find the libraries
+    if(NOT DEFINED libsearch)
+      set(libsearch ${search})
+    endif()
   endif()
+endforeach()
 
-  find_package_handle_standard_args(Unbound
-    REQUIRED_VARS UNBOUND_LIBRARY UNBOUND_INCLUDE_DIR
-    VERSION_VAR UNBOUND_VERSION)
-  if(Unbound_FOUND)
-    add_library(Unbound::Unbound UNKNOWN IMPORTED)
-    set_target_properties(Unbound::Unbound PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES "${UNBOUND_INCLUDE_DIR}"
-      IMPORTED_LOCATION "${UNBOUND_LIBRARY}")
-    set(UNBOUND_PC_REQUIRES)
-    set(UNBOUND_PC_LIBS "${UNBOUND_LIBRARY}")
-  endif()
+find_library(UNBOUND_LIBRARY NAMES "unbound" NAMES_PER_DIR ${${libsearch}}
+             PATH_SUFFIXES "lib" "${CMAKE_INSTALL_LIBDIR}")
+
+# Extract the version number from the header
+if(UNBOUND_INCLUDE_DIR AND EXISTS "${UNBOUND_INCLUDE_DIR}/unbound.h")
+  file(STRINGS "${UNBOUND_INCLUDE_DIR}/unbound.h" _major
+    REGEX "^ *# *define +UNBOUND_VERSION_MAJOR +[0-9]+.*$")
+  file(STRINGS "${UNBOUND_INCLUDE_DIR}/unbound.h" _minor
+    REGEX "^ *# *define +UNBOUND_VERSION_MINOR +[0-9]+.*$")
+  file(STRINGS "${UNBOUND_INCLUDE_DIR}/unbound.h" _micro
+    REGEX "^ *# *define +UNBOUND_VERSION_MICRO +[0-9]+.*$")
+  string(REGEX REPLACE "^[^0-9]+([0-9]+).*$" "\\1" _major ${_major})
+  string(REGEX REPLACE "^[^0-9]+([0-9]+).*$" "\\1" _minor ${_minor})
+  string(REGEX REPLACE "^[^0-9]+([0-9]+).*$" "\\1" _micro ${_micro})
+  set(UNBOUND_VERSION "${_major}.${_minor}.${_micro}")
 endif()
 
-mark_as_advanced(UNBOUND_INCLUDE_DIR)
-mark_as_advanced(UNBOUND_LIBRARY)
-
-# Restore the PKG_CONFIG_PATH environment variable
-if(PKG_CONFIG_FOUND)
-  set(ENV{PKG_CONFIG_PATH} ${_pkg_config_path})
+find_package_handle_standard_args(Unbound
+  REQUIRED_VARS UNBOUND_LIBRARY UNBOUND_INCLUDE_DIR
+  VERSION_VAR UNBOUND_VERSION)
+if(Unbound_FOUND)
+  add_library(Unbound::Unbound UNKNOWN IMPORTED)
+  set_target_properties(Unbound::Unbound PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${UNBOUND_INCLUDE_DIR}"
+    IMPORTED_LOCATION "${UNBOUND_LIBRARY}")
+  set(UNBOUND_PC_LIBS "${UNBOUND_LIBRARY}")
 endif()
